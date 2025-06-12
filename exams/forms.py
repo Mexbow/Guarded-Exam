4d44007c227d0bb4ace5689a0de4e312101b18c4
Mohamed Khaled Ahmed <120687246+Mexbow@users.noreply.github.com>
2025-06-12 12:29:53 +0300

    Delete exams directory

--- exams/forms.py
+++
@@ -1,8 +0,0 @@
-# exams/forms.py
-from django import forms
-from .models import Question
-
-class QuestionForm(forms.ModelForm):
-    class Meta:
-        model = Question
-        fields = ['text', 'question_type', 'options', 'answer']
